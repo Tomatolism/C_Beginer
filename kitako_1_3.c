9dc63d9d3789a09c5d31002445d37394934652c8
Tomatolism <iliketaiwan0826@gmail.com>
2023-06-12 16:21:02 +0900

    Create kitako_1_3.c

---
+++ kitako_1_3.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+void First(void){
+    printf("ふるいけや\n");
+}
+void Second(void){
+    printf("かわずとびこむ\n");
+}
+void Third(void){
+    printf("みずのおと\n");
+}
+
+
+int main()
+{
+    First();
+    Second();
+    Third();
+    return 0;
+}
