7758c1be4b9689bcb560a53c321af12ec6b8a7e1
Tomatolism <iliketaiwan0826@gmail.com>
2023-06-12 16:37:21 +0900

    Create kitako_3_11.c

---
+++ kitako_3_11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+    printf("Enter two integer number:");
+    scanf("%d %d",&x, &y);
+    printf("The average=%d \n", (x+y)/2);
+    
+    return 0;
+}
